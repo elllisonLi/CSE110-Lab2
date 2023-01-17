e41f3f2ba10b5354fc7bf3c549e533b4f9f0bac6
elllisonLi <yul054@ucsd.edu>
2023-01-17 13:46:35 -0800

    Update Everything

--- app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
@@ -1,2 +1,30 @@
-package edu.ucsd.cse110.lab2;public class BetterMainActivityTest {
+package edu.ucsd.cse110.lab2;
+
+import android.widget.TextView;
+import static org.junit.Assert.assertEquals;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class BetterMainActivityTest {
+    @Test
+    public void test_one_plus_one_equals_two() {
+
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            activity.findViewById(R.id.btn_one).performClick();
+            activity.findViewById(R.id.btn_plus).performClick();
+            activity.findViewById(R.id.btn_one).performClick();
+            activity.findViewById(R.id.btn_equals).performClick();
+            assertEquals(2, Integer.parseInt(((TextView)activity.findViewById(R.id.display)).getText().toString()));
+        });
+    }
 }
